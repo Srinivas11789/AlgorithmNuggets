85cb82b5115aa6f4beff93afeaa6391cc23ff961
Srinivas <spg349@nyu.edu>
2018-03-17 12:35:14 -0400

    :sunrise: morninf first cook - bubble sort

--- ProblemSolving/sorting/bubbleSort.py
+++ ProblemSolving/sorting/bubbleSort.py
@@ -14,4 +14,27 @@ for i in range(n):
 print "Array is sorted in "+str(swaps)+" swaps."
 print "First Element: "+str(a[0])
 print "Last Element: "+str(a[n-1])
-            
+
+#### Morning cookies Bubble sort
+"""
+#!/bin/python
+
+import sys
+
+n = int(raw_input().strip())
+a = map(int, raw_input().strip().split(' '))
+# Write Your Code Here
+
+temp = None
+swaps = 0
+for i in range(n):
+    for j in range(n-1):
+        if a[j] > a[j+1]:
+            temp = a[j]
+            a[j] = a[j+1]
+            a[j+1] = temp
+            swaps += 1
+print "Array is sorted in "+str(swaps)+" swaps."
+print "First Element: "+str(a[0])
+print "Last Element: "+str(a[len(a)-1])
+"""            
