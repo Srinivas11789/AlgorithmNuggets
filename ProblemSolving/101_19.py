afeb26f53db70bf1d0a5de422ba7bc0c96f1e2aa
Srinivas <spg349@nyu.edu>
2018-03-18 01:03:58 -0400

    :neckbeard: solution based on and logic from discussions hackerrank solves timeout

--- ProblemSolving/101_19.py
+++ ProblemSolving/101_19.py
@@ -1,5 +1,5 @@
 # Bitwise AND operation - Timeout error
-
+# SOLUTION from Jakus hackerrank passes all the testcase, based on the strucuture of AND logic
 #!/bin/python
 
 import sys
@@ -9,6 +9,11 @@ t = int(raw_input().strip())
 for a0 in xrange(t):
     n,k = raw_input().strip().split(' ')
     n,k = [int(n),int(k)]
+    # Logic based on Jakus explanation in hackerank 
+    # if k is odd, k-1 is even, k-1 & k == k-1 and k-1 | k == k
+    # if k is even, k-1 is odd, k-1 can be reached if k-1 | k <= n
+    print k-1 if (k-1 | k) <= n else k-2
+    """
     maxi = 0
     initk = k
     for i in range(1,n):
@@ -20,3 +25,4 @@ for a0 in xrange(t):
                     break
                 k += 1
     print maxi
+    """
