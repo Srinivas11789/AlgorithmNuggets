e5041cfb9525f32a4fb34f89544085b88b2ab27c
Srinivas <spg349@nyu.edu>
2018-03-17 22:23:40 -0400

    :+1: testcase generation

---
+++ ProblemSolving/101_18.py
@@ -0,0 +1,17 @@
+# Task to generate testcase for a problem
+# Simple Testcase
+# Number of testcases
+print("2")
+# number of students and threshold
+print("10 5")
+# Array of studen arrival times
+times = " ".join([str(i) for i in range(9)])
+times += " -5"
+print(times)
+# number of students and threshold
+print("50 25")
+# Array of studen arrival times
+times = " ".join([str(i) for i in range(49)])
+times += " -5"
+print(times)
+    
