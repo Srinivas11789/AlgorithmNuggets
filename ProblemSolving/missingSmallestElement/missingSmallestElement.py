f2133cd86d8f1c960b3655e14c849b8db3cebbf5
Srinivas <spg349@nyu.edu>
2018-03-20 23:45:42 -0400

    :bridge_at_night: missing element codility problem

--- ProblemSolving/missingSmallestElement/missingSmallestElement.py
+++ ProblemSolving/missingSmallestElement/missingSmallestElement.py
@@ -1,18 +1,22 @@
 ### Codility Problem Missing Integer
-# * Always consider a dictionary or list method of solving
-
+# * Always consider a dictionary or list !!!!!!!!!!!!!!(Add SET to the list)!!!!!!!!!!!!!!!! method of solving
+# * when using python try to use inbuild data structures like list, set and dictionary to perform operation making it simple
 # you can write to stdout for debugging purposes, e.g.
 # print("this is a debug message")
 
 def solution(A):
     # write your code in Python 3.6
-    
-    
-    
-    
+    n = len(A)
+    check = set(A)
+    for i in range(1,n+2):
+        #print(i)
+        if i not in check:
+            return i
+    return -1
+    """
     # Boolean array technique
     # After correcting negative - 40 percent correct
-    
+
     n = len(A)
     check = [0 for i in range(n)]
     small = 600000
@@ -24,9 +28,10 @@ def solution(A):
         if A[i]-1 not in A:
             if A[i]-1 != 0:
                 if A[i]-1 < small:
-                   small = A[i]-1 
+                   small = A[i]-1
         if A[i]+1 not in A:
             if A[i]+1 != 0:
                 if A[i]+1 < small:
-                   small = A[i]+1 
+                   small = A[i]+1
     return small
+~   """           
