a6e29ec1ea5def33b0ac99e2aee6ba77d46f70fb
Srinivas <spg349@nyu.edu>
2018-03-21 19:25:51 -0400

    :bridge_at_night::snowman: codility dominator (2 testcase remain 80%)

--- ProblemSolving/dominator/dominator.py
+++ ProblemSolving/dominator/dominator.py
@@ -1,8 +1,66 @@
 # Codility Dominator -
 # Lessons learnt: 
 ## Easy testcase failed - test for empty array!!!!!!!!
+## Two testcases keep failing for random and large number 85+ all param pass
+
+# A Linear Time Majority Vote Algorithm - Good exaplanation at http://www.cs.utexas.edu/~moore/best-ideas/mjrty/
 
 def solution(A):
+    
+    ## Majorioty Vote Problem Idea
+    ## Have two variables one ind keeps track of the element and other the occurence, iterate the array one time and increment for occurence and decrement for not occuring scenario
+    
+    n = len(A)
+    ind = 0
+    count = 0
+    last_ind = 0
+    maxi = 0
+    
+    if n == 0:
+        return -1
+    
+    for i in range(n):
+    # This logic will work according to the explanation, This will give the max element for sure  
+        if count == 0:
+            count += 1
+            ind = A[i]
+            last_ind = i
+        else:
+            count -= 1
+    # we got to find whether the given element is greater than half the array length
+    for i in range(n):
+        if A[i] == ind:
+            maxi += 1
+            #last_ind = i
+            
+    #if float(max) > n/2.0:
+    if maxi > n//2:
+        return last_ind
+    else:
+        return -1
+        
+    """
+    # 50 percent on all param 
+    ### Throwing the last index
+    n = len(A)
+    ind = 0
+    max = 0
+    count = [0 for i in range(n)]
+    if n == 0:
+        return -1
+    for i in range(n):
+        count[A[i]] += 1
+        if count[A[i]] > max:
+            max = count[A[i]]
+            ind = i
+    if float(max) > n/2.0:
+        return ind
+    else:
+        return -1
+    """
+    
+    """
+    ### Passes for 75 to 80 percent on all params
     # Dictionary method
     n = len(A)
     count = {}
@@ -16,8 +74,9 @@ def solution(A):
         count[A[i]][1].append(i)
         if count[A[i]][0] > max:
             max = A[i]
-    #print(count[max][0],n//2)
-    if count[max][0] > n//2:
+    #print(count[max][0],n/2)
+    if float(count[max][0]) > n/2.0:
         return count[max][1][0]
     else:
         return -1
+    """    
