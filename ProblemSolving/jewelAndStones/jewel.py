9544f3a8853525ed803b2f4f925bbe13d11132fc
Srinivas <spg349@nyu.edu>
2018-04-14 14:13:09 -0400

    :sunrise::city_sunrise: jewel amnd stone - do more

---
+++ ProblemSolving/jewelAndStones/jewel.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def numJewelsInStones(self, J, S):
+        """
+        :type J: str
+        :type S: str
+        :rtype: int
+        """
+        
+        """
+        # 100 pass 
+        
+        count = 0
+        
+        for stone in S:
+            if stone in J:
+                count += J.count(stone)
+        
+        return count
+        """
+        
+        # Not so pythonic solution - 100
+        count = 0
+        
+        for i in range(len(S)):
+            if S[i] in J:
+                count += 1
+        return count
