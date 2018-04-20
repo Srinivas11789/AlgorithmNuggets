31814aaf26957260f0defa8a55f5853ebcefee11
Srinivas <spg349@nyu.edu>
2018-04-20 15:27:42 -0400

    :city_sunrise::sunrise: intersection of 2 arrays

---
+++ ProblemSolving/intersectionOfTwoArray/inter.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def intersection(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+
+        str1 = "".join(str(i) for i in nums1) 
+        str2 = "".join(str(i) for i in nums2)
+        if str1 == str2:
+            return map(int,str1)
+        elif str1 == "" or str2 == "":
+            return []
+        #if str2 in str1:
+        #    return map(int,str1[str1.index(str2)])
+        else:
+            result = set()
+            for num in nums1:
+                if num in nums2:
+                    result.add(num)
+            return list(result)
+
