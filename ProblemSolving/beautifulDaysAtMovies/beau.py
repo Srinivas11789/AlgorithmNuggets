2cc5bfac22fa397999e03f5fc48d79d4c1cd6e63
Srinivas <spg349@nyu.edu>
2018-05-05 18:51:33 -0400

    :city_sunset: beau day at movies program

---
+++ ProblemSolving/beautifulDaysAtMovies/beau.py
@@ -0,0 +1,19 @@
+#!/bin/python
+
+import sys
+
+def beautifulDays(i, j, k):
+    # Complete this function
+    count = 0
+    for i in range(i,j+1):
+        if (abs(i-int(str(i)[::-1]))/float(k)).is_integer():
+            count += 1
+    return count
+        
+
+if __name__ == "__main__":
+    i, j, k = raw_input().strip().split(' ')
+    i, j, k = [int(i), int(j), int(k)]
+    result = beautifulDays(i, j, k)
+    print result
+
