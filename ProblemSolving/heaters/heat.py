65c0670227b70d8f458123ac2f79673ca8aa9246
Srinivas <spg349@nyu.edu>
2018-05-06 18:47:14 -0400

    :city_sunset: heaters problem

--- ProblemSolving/heaters/heat.py
+++ ProblemSolving/heaters/heat.py
@@ -6,7 +6,7 @@ class Solution(object):
         :rtype: int
         """
         
-        
+        """
         # Works - but a scale cases of [282475249,622650073,984943658,144108930,470211272,101027544,457850878,458777923]
 #[823564440,115438165,784484492,74243042,114807987,137522503,441282327,16531729,823378840,143542612] fails the logic with memory error
         # Logic - For every heater placement add the heater it covers and check whether its equal to all the house array, try with radius 1 and ....
@@ -25,6 +25,38 @@ class Solution(object):
                 print covered
                 return radius
         return -1
+        """
+        
+        # Alternate from discussions
+        
+        # Sort the houses and heaters
+        heaters = sorted(heaters)
+        houses = sorted(houses)
+        
+        # Variable declaration
+        radius = i = 0
+        
+        for house in houses:
+            while i < len(heaters) and heaters[i] < house:
+                i += 1
+                
+            # When heater is placed in front of all the houses, then 0
+            if i == 0:
+                radius = max(radius, heaters[i] - house)
+            # All the possibility of heaters have been exhausted
+            if i == len(heaters):
+                return max(radius, houses[-1] - heaters[-1])
+            else:
+                radius = max(radius, min(heaters[i] - house, house- heaters[i-1]))
+        return radius
+    
+ ################# Pending to understand and explore more 
+                
+        
+        
+        
+        
+        
         
         
         
