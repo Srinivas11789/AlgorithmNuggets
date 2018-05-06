f5199acc27a497fe2dacbbeb21f58f0cb6038f2e
Srinivas <spg349@nyu.edu>
2018-05-06 15:33:04 -0400

    :city_sunset: rotate list

---
+++ ProblemSolving/rotateList/rot.py
@@ -0,0 +1,62 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    
+    def rotateRight(self, head, k):
+        """
+        :type head: ListNode
+        :type k: int
+        :rtype: ListNode
+        """
+        
+        # Nice logic - Make the list a circular list and perform the operations
+        
+        # Only head handle
+        if not head:
+            return head
+        if k == 0:
+            return head
+        if not head.next:
+            return head
+        
+        # Find the last node and make it circular list
+        
+        # Store the first list element
+        first = head
+        length = 1
+        while first.next != None:
+            length += 1
+            first = first.next
+
+        # Make k within the range of the list length
+        k = (length-k)%length
+
+        if k == 0:
+            # Break the circularity before returning
+            return head
+        else:
+            # Make it a circular list
+            first.next = head
+        
+        # Goto kth position and break the list from being circular
+        i = 1
+        first = head
+        while i < k:
+            i += 1
+            first = first.next
+        
+        head = first.next
+        first.next = None
+        
+        return head
+            
+        
+        
+        
+        
+        
+        
