fe7c4bc50cbc0656779d08956291e94a16b6e6c4
Srinivas <spg349@nyu.edu>
2018-05-14 16:39:55 -0400

    :city_sunset: resolved

--- ProblemSolving/findAllNumbersDisappearedInAnArray/arr.py
+++ ProblemSolving/findAllNumbersDisappearedInAnArray/arr.py
@@ -1,10 +1,29 @@
-#Pending....
 class Solution(object):
     def findDisappearedNumbers(self, nums):
         """
         :type nums: List[int]
         :rtype: List[int]
         """
+        
+        # 1. Try with O(n) run-time and O(1) space -Pending
+        
+        
+        # 2. 100pass - Most of the helpers have one constant space present to carry out the operation
+
+        # Logic -- referencing index as iteration and converting values such that we know if it was visited or not. Leverage indices vs values logic. Convert all the visited values to corresponding indices in (0-n) form by subtracting 1 and change the corresponding value to negative. Hence for a value 2 present in array, we have array index 2-1=1 a negative value.
+        
+        # Constant space
+        result = []
+        
+        for num in nums:
+            nums[abs(num)-1] = abs(nums[abs(num)-1])*-1
+
+        # Looking for positive values and getting the corresponding index+1 gives the result
+        for i in range(len(nums)):
+            if nums[i] > 0:
+                result.append(i+1)
+        
+        return result
         
         
         """
