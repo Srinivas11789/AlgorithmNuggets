ae246125759bdb58fe04f4e57dc0cfcf615a54bb
Srinivas <spg349@nyu.edu>
2018-06-16 14:36:06 -0400

    :city_sunrise: custom sort string

--- ProblemSolving/customSortString/sort.py
+++ ProblemSolving/customSortString/sort.py
@@ -1,6 +1,3 @@
-# Pending...
-# Tc fail - duplicate character occurence
-
 class Solution(object):
     def customSortString(self, S, T):
         """
@@ -8,7 +5,32 @@ class Solution(object):
         :type T: str
         :rtype: str
         """
-    
+        
+        # Use a differernt/custom key for the sorted function with reference to the given array S
+        # 100 pass
+        def keys(char):
+            if char in S:
+                return S.index(char)
+            else:
+                # Return anything higher will suggest the sorted function to place at the end - 1000, or len(T) or 26
+                return len(T)
+        
+        return "".join(sorted(T, key=keys))
+        
+        
+        
+        """
+        # Construct a sorted alp with Only lower case characters then solve
+        key = ["a"]
+        for i in range(25):
+            key.append(chr(ord(key[-1])+1))
+        start = key.index(S[0])
+        key = key[start:]+key[:start]
+          
+        return sorted(T,key=key)
+        """    
+        
+        """
         # Logic 1 - Any combination - so add the array S in the same order with others
         result = []
         match = []
@@ -17,18 +39,20 @@ class Solution(object):
                 result.append(char)
             else:
                 match.append(char)
-                
-        print match
         
         i = 0
         S = list(S)
         while i < len(S):
             if S[i] not in match:
                 del S[i]
+            elif match.count(S[i]) > 1:
+                S = S[:i]+[S[i]]*(match.count(S[i])-1)+S[i:]
+                i += match.count(S[i])-1
             else:
                 i += 1
         
         return "".join(result)+"".join(S)
+        """
         
         """
         # One Logic of iteration 
