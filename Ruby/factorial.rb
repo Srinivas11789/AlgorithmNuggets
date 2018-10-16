abefa19d95ade08841425396dd685b4c05903205
Preethi <preethi900@gmail.com>
2018-10-16 12:19:31 -0700

    Adding comments for leap year factoring and reverse programs

--- Ruby/factorial.rb
+++ Ruby/factorial.rb
@@ -1,9 +1,11 @@
 #Ruby Factorial program 
-
+#Function to call the factorial of the program
 def fact(n)
+#Check to return 1 if the given number is 0
  if n == 0
   1
  else
+#Condition to find the factorial of a program
   if n> 0
    n * fact(n-1)
    
@@ -12,5 +14,7 @@ def fact(n)
   end
 end   
 end
-
+#Getting the input from user
+input = Integer(gets.chomp)
+#print statement to print the factorial
 puts fact(ARGV[0].to_i)
--- Ruby/leap.rb
+++ Ruby/leap.rb
@@ -1,14 +1,24 @@
+#Program to find if the year is a leap year or not
+
+#Get the starting year from user
 puts "Enter starting year:"
+
+
         starting_year = gets.chomp.to_i
+#Get the ending year from user
 puts "Enter ending year:"
       ending_year = gets.chomp.to_i
+#variable assignment with user input
 year = starting_year
 while true
+#checks to find if its a leap year or not
       if year%4==0
           if year%100!=0 || year%400 ==0
               puts year.to_s + ' is a Leap Year'
           end
       end
+#Incrementing year
     year = year +1
+#Breaking the loop once the end year has been reached
       break if year >= ending_year
 end
