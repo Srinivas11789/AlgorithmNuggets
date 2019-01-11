0e2393b460457bd1a7ef617005032a201a39ccd0
Srinivas <spg349@nyu.edu>
2019-01-11 04:21:07 -0800

    lt easy - nary tree depth recursive + iterative == 100% pass

--- ProblemSolving/maxHeightNaryTree/max.py
+++ ProblemSolving/maxHeightNaryTree/max.py
@@ -12,6 +12,7 @@ class Solution(object):
         :rtype: int
         """
         
+        
         # Recursive Method of finding depth
         def depth(node):
             
@@ -32,3 +33,70 @@ class Solution(object):
             return 0
         
         return depth(root)
+        
+        
+        """
+        # Iterative method -- 100% pass
+        
+        # Fill in each level (BFS mode) as a list of levels of a tree
+        # -- The length of this (list of list/levels) will be the maximum depth
+        
+        # Null check
+        if not root:
+            return 0
+        
+        # Traverse levels helper
+        stack = [[root]]
+        
+        # Levels list --- values at each level
+        levels = []
+        
+        # Iterate until exhaust
+        while stack:
+            
+            # current level to traverse in list
+            current_level = stack.pop(0)
+            
+            # current levels values list
+            levels.append([])
+            
+            # Next level list of children
+            next_level = []
+                
+            # Exhaust current level list (all the current nodes)
+            while current_level:
+                # Remove node in each level (left to right)
+                current_node = current_level.pop(0)
+                
+                # Update the current node values list (always the last in the list for that level)
+                levels[-1].append(current_node.val)
+                
+                # Add all children to next level for traversing
+                if current_node.children:
+                    next_level.extend(current_node.children)
+            
+            # If there are nodes to traverse in the next level
+            if next_level:
+                stack.append(next_level)
+                    
+        #print levels
+        
+        # Depth
+        return len(levels)
+        """    
+            
+        """
+        # Helpers/THoughts
+            i, n = 0, len(prev_level)
+            next_level = []
+            while i < n:
+                current = prev_level[i]
+                if current.children:
+                    stack.append([])
+                    for child in current.children:
+                        stack[-1].append(child)
+                        next_level.append(child.val)
+                i += 1
+            stack.append(next_level)
+        """
+        
