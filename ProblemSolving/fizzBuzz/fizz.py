0d41f65e3dc9f575932fb68ebdb6564b15c969b8
Srinivas <spg349@nyu.edu>
2019-01-20 13:40:17 -0800

    fizzBuzz update

--- ProblemSolving/fizzBuzz/fizz.py
+++ ProblemSolving/fizzBuzz/fizz.py
@@ -17,4 +17,28 @@ class Solution(object):
                 result.append(str(i))
         return result
     
+# Alternate Logic
+"""
+class Solution(object):
+    def fizzBuzz(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
         
+        result = []
+        
+        for i in range(1,n+1):
+            current = ""
+            # Divisible by 3
+            if i%3 == 0:
+                current += "Fizz"
+             # Divisible by 5
+            if i%5 == 0:
+                current += "Buzz"
+            # Not visible by Both
+            if not current:
+                current = str(i)
+            result.append(current)
+        return result
+"""        
