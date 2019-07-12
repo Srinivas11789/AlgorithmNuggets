3e1e800dbf31737181cd86800d88306a020449ce
srinivas <spg349@nyu.edu>
2019-07-12 00:23:12 -0700

    :rocket: update duplicate0s with extra logic - lt easy - 100 pass 97% faster

--- ProblemSolving/duplicateZeros/duplicate.py
+++ ProblemSolving/duplicateZeros/duplicate.py
@@ -7,6 +7,7 @@ class Solution(object):
         
         # Logic1: Lousy method: 100 pass 5% faster
         # * At every 0 hit, shift all the elements and add zero
+        """
         n = len(arr)
         i = 0
         while i < n:
@@ -20,35 +21,86 @@ class Solution(object):
                 i += 2
             else:
                 i += 1
+        """
         
-        # Logic2: Pending...
+        # Logic2: Similar to 2 pointer method - 100 pass 97 % faster
         # * We could count the zeros (shift) that will fit the array
         # * Use the remaining array to make the changes
-        """
+
         n = len(arr)
-        count = 0
+        
+        # Count the Number of shifts - I altered this to store appropriate positions
+        shift_indices = [] # Indices where shift should happen
+        shift = 0 # Count the shift required as we iterate
+        shift_index = 0 # The index that would be the end of array in the future shifted array
+        for i in range(n):
+            shift += 1 # Shift for every element
+            if shift == n: # If shift cover the array return the shift index
+                shift_index = i
+                break
+            elif arr[i] == 0: # Extra shift for 0s
+                shift_indices.append(i)
+                shift += 1
+            if shift == n: # If shift cover the array return the shift index ( after extra shift)
+                shift_index = i
+                break
+        # print shift, shift_index, arr, shift_indices
+        
+        # The real index in the array
+        real_index = n-1
+        # The next shift index
+        if shift_indices:
+            next_shift = shift_indices.pop()
+        else:
+            next_shift = None
+        # Shifting... by one iteration
+        while shift_index >= 0 and real_index >= 0:
+                arr[real_index] = arr[shift_index] # Shift for all the elements
+                if shift_index == next_shift: # Add extra 0s in place for next shift element
+                    arr[real_index-1] = 0
+                    real_index -= 1 # Extra iteration for extra 0
+                    # Update the next shift
+                    if shift_indices:
+                        next_shift = shift_indices.pop()
+                    else:
+                        next_shift = None
+                # Iterate and reduce indices
+                shift_index -= 1
+                real_index -= 1
+        
+        """
+        # Uncleaned attempt
+        n = len(arr)
+        shift_indices = []
         shift = 0
         shift_index = 0
-        
-	# Count the zeros, shift and figure out the shift index
-	for i in range(n):
+        for i in range(n):
             shift += 1
             if shift == n:
                 shift_index = i
                 break
             elif arr[i] == 0:
-                count += 1
+                shift_indices.append(i)
                 shift += 1
-            print shift, shift_index
-
-	# Iterate once and shift elements 
+            if shift == n:
+                shift_index = i
+                break
+        print shift, shift_index, arr, shift_indices
         real_index = n-1
+        if shift_indices:
+            next_shift = shift_indices.pop()
+        else:
+            next_shift = None
         while shift_index >= 0 and real_index >= 0:
                 arr[real_index] = arr[shift_index] 
-                if arr[real_index] == 0:
+                if shift_index == next_shift:
                     arr[real_index-1] = 0
                     real_index -= 1
+                    if shift_indices:
+                        next_shift = shift_indices.pop()
+                    else:
+                        next_shift = None
                 shift_index -= 1
                 real_index -= 1
-                print arr
         """
+        
