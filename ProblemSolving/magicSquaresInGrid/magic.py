a331f33824b2e7583348851756a90411bb194561
sri <spg349@nyu.edu>
2019-12-16 12:23:33 -0800

    :construction_worker: fixes to magic squares problem - 100 pass

--- ProblemSolving/magicSquaresInGrid/magic.py
+++ ProblemSolving/magicSquaresInGrid/magic.py
@@ -36,6 +36,9 @@ class Solution(object):
                 # column sum
                 col_sum = 0
                 for r in range(len(matrix)):
+                    # Distinct digits from 1 to 9 only rule
+                    if matrix[r][c] < 1 or matrix[r][c] > 9:
+                        return False
                     col_sum += matrix[r][c]
                 if col_sum != check:
                     return False
@@ -69,14 +72,18 @@ class Solution(object):
         
         count = 0
         r = 0
-        for c in range(len(grid[0])):
-            #print(r, c, r+2, c+2)
-            if r+2 < len(grid) and c+2 < len(grid[0]):
-                new_matrix = []
-                for row in range(r, r+3):
-                    new_matrix.append(grid[row][c:c+3])
-                if is_magic_square(new_matrix):
-                    count += 1
+        while r < len(grid):
+            for c in range(len(grid[0])):
+                #print(r, c, r+2, c+2)
+                if r+2 < len(grid) and c+2 < len(grid[0]):
+                    new_matrix = []
+                    for row in range(r, r+3):
+                        new_matrix.append(grid[row][c:c+3])
+                    if is_magic_square(new_matrix):
+                            count += 1
+            r += 1
         return count
+    
+        # Note: [[10,3,5],[1,6,11],[7,9,2]] was a correct magic square but violates the question of 1 to 9 distinct digits so the answer is False, this was very confusing to me as it was a magic square already with all sum equalities
     
         
